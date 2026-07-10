9c3fd3764e756ffed13bb58792621e12e276e4dc
24f2002719 <24f2002719@ds.study.iitm.ac.in>
2026-07-10 13:35:12 +0530

    Final Draft3

--- src/views/admin/FacilityManagement.vue
+++ src/views/admin/FacilityManagement.vue
@@ -381,15 +381,7 @@
           </select>
         </div>
 
-        <div class="space-y-1 text-xs">
-          <label class="block font-semibold text-on-surface-variant">Availability Time Details</label>
-          <input v-model="editFacilityForm.time" class="w-full bg-slate-50 border border-outline-variant rounded px-3 py-2.5 outline-none focus:border-primary" placeholder="e.g. Open now until 6:00 PM" required />
-        </div>
 
-        <div class="space-y-1 text-xs">
-          <label class="block font-semibold text-on-surface-variant">Next Booking Details</label>
-          <input v-model="editFacilityForm.next" class="w-full bg-slate-50 border border-outline-variant rounded px-3 py-2.5 outline-none focus:border-primary" placeholder="e.g. Open" />
-        </div>
 
         <div class="space-y-1 text-xs">
           <label class="block font-semibold text-on-surface-variant">Accent Color Picker</label>
